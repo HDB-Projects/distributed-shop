b06554c2c6f25177f33831eb5cff44d98a000f55
HDB <276074700+HDB-Projects@users.noreply.github.com>
2026-04-17 05:14:55 +0200

    OrderController initial implementation

--- services/order-service/Controllers/OrderController.cs
+++ services/order-service/Controllers/OrderController.cs
@@ -1,6 +1,24 @@
-﻿namespace OrderService;
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderService.Models;
+using OSS = OrderService.Services;
 
-public class OrderController
+namespace OrderService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrderController : ControllerBase
 {
+    private readonly OSS.IOrderService _orderService;
 
+    public OrderController(OSS.IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+
+    [HttpGet]
+    public ActionResult<List<CustomerOrder>> Get()
+    {
+        List<CustomerOrder> orders = _orderService.GetAllOrders();
+        return Ok(orders);
+    }
 }
