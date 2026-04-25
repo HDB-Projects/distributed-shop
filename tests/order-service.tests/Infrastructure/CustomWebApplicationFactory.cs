47d42b03d9da98fb5db276d0487e6009100c4ce9
HDB <276074700+HDB-Projects@users.noreply.github.com>
2026-04-25 20:51:15 +0200

    Added Setup for Intergration Tests

---
+++ tests/order-service.tests/Infrastructure/CustomWebApplicationFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OrderService.Data;
+
+namespace OrderService.Tests.Infrastructure;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder webHostBuilder)
+    {
+        webHostBuilder.ConfigureServices(services =>
+        {
+            ServiceDescriptor? descriptor = services.SingleOrDefault(d =>
+            {
+                return d.ServiceType == typeof(DbContextOptions<OrderDbContext>);
+            });
+
+            if (descriptor != null)
+                services.Remove(descriptor);
+
+            services.AddDbContext<OrderDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("TestDb");
+            });    
+        });
+    }
+}
---
+++ tests/order-service.tests/Integration/OrderApiTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using System.Net.Http.Json;
+using OrderService.Models;
+using NUnit.Framework;
+using OrderService.Tests.Infrastructure;
+
+namespace OrderService.Tests.Integraton;
+
+[TestFixture]
+public class OrderApiTests
+{
+    private WebApplicationFactory<Program> _factory;
+    private HttpClient _client;
+
+    [SetUp]
+    public void Setup()
+    {
+        _factory = new CustomWebApplicationFactory();
+        _client = _factory.CreateClient();
+    }
+    [TearDown]
+    public void TearDown()
+    {
+        _client.Dispose();
+        _factory.Dispose();
+    }
+
+    [Test]
+    public async Task Post_Order_Should_Return_Created()
+    {
+        // Arrange
+        CreateOrderDto createOrderDto = new CreateOrderDto
+        {
+            UserId = Guid.NewGuid(),
+            TotalAmount = 10
+        };
+
+        // Act
+        HttpResponseMessage response = await _client.PostAsJsonAsync("/api/order", createOrderDto);
+        CustomerOrder? result = await response.Content.ReadFromJsonAsync<CustomerOrder>();
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result!.TotalAmount, Is.EqualTo(10));
+    }
+}
